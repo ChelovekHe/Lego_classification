67587cd673193dd23169816fe2de7f7a39feed4b
harrysocool <harry_so_cool@live.com>
2016-04-22 15:00:28 +0100

    use color split and contours to find the logo icon area

--- src/draft.py
+++ src/draft.py
@@ -4,7 +4,8 @@ import numpy as np
 import cv2
 
 # load the image and convert it to grayscale
-image = cv2.imread('../fig/Box 3/WP_20160313_13_03_08_Pro_LI.jpg')
+image = cv2.imread('../fig/Box 5/WP_20160313_13_01_14_Pro_LI.jpg')
+image = cv2.resize(image,(0,0),fx = 0.2,fy = 0.2)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # compute the Scharr gradient magnitude representation of the images
@@ -17,7 +18,7 @@ gradient = cv2.subtract(gradX, gradY)
 gradient = cv2.convertScaleAbs(gradient)
 
 # blur and threshold the image
-blurred = cv2.blur(gradient, (9, 9))
+blurred = cv2.blur(gradient, (5, 5))
 (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 
 # construct a closing kernel and apply it to the thresholded image
@@ -28,22 +29,18 @@ closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 closed = cv2.erode(closed, None, iterations = 4)
 closed = cv2.dilate(closed, None, iterations = 4)
 
+# find the contours in the thresholded image, then sort the contours
+# by their area, keeping only the largest one
+(_, cnts, _) = cv2.findContours(closed.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
 
-cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-cv2.imshow("Image", thresh)
-cv2.resizeWindow("Image",1000,1000)
+# compute the rotated bounding box of the largest contour
+rect = cv2.minAreaRect(c)
+box = np.int0(cv2.boxPoints(rect))
+
+# draw a bounding box arounded the detected barcode and display the
+# image
+
+cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+cv2.imshow("Image", image)
 cv2.waitKey(0)
-# # find the contours in the thresholded image, then sort the contours
-# # by their area, keeping only the largest one
-# (cnts, _) = cv2.findContours(closed.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-# c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
-#
-# # compute the rotated bounding box of the largest contour
-# rect = cv2.minAreaRect(c)
-# box = np.int0(cv2.BoxPoints(rect))
-#
-# # draw a bounding box arounded the detected barcode and display the
-# # image
-# cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
-# cv2.imshow("Image", image)
-# cv2.waitKey(0)
