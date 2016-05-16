614c7a40acab1216cfb09739c7d1e681ecbffe51
harrysocool <harry_so_cool@live.com>
2016-05-16 11:16:52 +0100

    delete conflits

--- src/video.py
+++ src/video.py
@@ -4,49 +4,9 @@ from tesserwrap import Tesseract
 from Lego.Lego import Lego
 from Lego.imgPreprocessing import LogoAffinePos
 
-
-<<<<<<< HEAD
-VWIDTH = 640
-VHIGH = 480
-cap = cv2.VideoCapture(0)
-ret = cap.set(3, VWIDTH)
-ret = cap.set(4, VHIGH)
-
-while 1:
-    _, frame = cap.read()
-
-    imgH,imgW, _ = frame.shape
-
-    l = Lego(frame)
-    logo_box = l.get_logo_box()
-    info = l.get_information_part()
-
-    cv2.drawContours(frame, [logo_box], -1, (0, 255, 0), 2)
-
-    if l._has_information:
-        cv2.imshow('info', info)
-        cv2.imwrite('info.jpg',info)
-        img = Image.open('info.jpg')
-        tr = Tesseract(datadir='../tessdata', lang='eng')
-        text = tr.ocr_image(img)
-        print(text)
-    # if l._has_rotated_image:
-    #     img = l._rotated_image
-    #     img[l._logo_center_y:l._logo_center_y+3, l._logo_center_x:l._logo_center_x+3, :] = [255, 255, 255]
-    #     cv2.imshow('rotate', img)
-    # if l._has_potential_logo:
-    #     cv2.imshow('logo', l.get_logo_image())
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-cap.release()
-cv2.destroyAllWindows()
-=======
 def resize(image, factor=0.5):
     image = cv2.resize(image, (0, 0), fx=factor, fy=factor)
     return image
-
 
 def ocr(info):
     info = resize(info, 0.5)
@@ -109,4 +69,3 @@ if __name__ == '__main__':
             break
     cap.release()
     cv2.destroyAllWindows()
->>>>>>> dev
